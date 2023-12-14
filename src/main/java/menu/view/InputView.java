85d66cf2d34cb10c79f3da7556c3e9127aaf5213
5jeong <wjdgns4385@naver.com>
2023-12-15 01:58:11 +0900

    [Refactor] 입력메세지 추가

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -6,12 +6,16 @@ import menu.util.ConvertUtil;
 public class InputView {
 
     public List<String> inputCoachNames(){
+        System.out.println();
+        System.out.println("코치의 이름을 입력해 주세요. (, 로 구분)");
         String coachNames = Console.readLine();
         InputViewValidator.validateCoachNames(coachNames);
         return ConvertUtil.convertStringToList(coachNames);
     }
 
-    public List<String> noEatMenu(){
+    public List<String> noEatMenu(String name){
+        System.out.println();
+        System.out.println(String.format("%s(이)가 못 먹는 메뉴를 입력해 주세요.",name));
         String noEatMenu = Console.readLine();
         InputViewValidator.validateNoEatMenu(noEatMenu);
         return ConvertUtil.convertStringToList(noEatMenu);
