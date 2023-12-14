987aa0228b8bb7dd284701479b773c68ad28007d
5jeong <wjdgns4385@naver.com>
2023-12-15 03:15:44 +0900

    [Feat] 추천메뉴 추가 기능 구현

--- src/main/java/menu/domain/Coach.java
+++ src/main/java/menu/domain/Coach.java
@@ -2,6 +2,7 @@ package menu.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Coach {
 
@@ -14,13 +15,43 @@ public class Coach {
         this.name = name;
     }
 
-    public void addNoEatMenus(List<String> menus){
-        for(String menu : menus){
+    public void addNoEatMenus(List<String> menus) {
+        for (String menu : menus) {
             noEatMenus.add(menu);
         }
     }
 
+    public void addRecommendMenus(String menu) {
+        recommendMenus.add(menu);
+    }
+
+    public boolean eatableMenu(String menu) {
+        return !noEatMenu(menu) || !isDuplicateMenu(menu);
+    }
+
+    public boolean noEatMenu(String menu) {
+        for (String noEatMenu : noEatMenus) {
+            if (menu.equals(noEatMenu)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean isDuplicateMenu(String menu) {
+        for (String recommedMenu : recommendMenus) {
+            if (recommedMenu.equals(menu)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public String getName() {
         return name;
+    }
+
+    public String recommendMenu() {
+        return "[ " + name + " | " + recommendMenus.stream().collect(Collectors.joining(" | ")) + " ]";
     }
 }
